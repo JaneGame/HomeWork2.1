c69d2579ba59d186a9e82cfb0c3dfa8fbc6b96aa
JaneGame <fibi92@mail.ru>
2021-08-11 16:31:32 +0300

    Programm

---
+++ Money.java
@@ -0,0 +1,9 @@
+class Main {
+  public static void main(String[] args) {
+    int moneyStart = 2_000_000_000;
+    int translation = 500_000_000;
+    int moneyFinish = (moneyStart+translation);
+    System.out.println(moneyFinish);
+
+  }
+}
